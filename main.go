ff692ab597eddbc4453b68fc8435d78f4c21936f
Piotr Zduniak <piotr@zduniak.net>
2015-04-09 09:38:02 +0200

    Added a kiri agent

--- main.go
+++ main.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"log"
+	"strings"
 
 	r "github.com/dancannon/gorethink"
 	"github.com/lavab/flag"
+	"github.com/lavab/kiri"
 	"github.com/rs/cors"
 	"github.com/zenazn/goji"
 )
@@ -15,6 +17,15 @@ var (
 	rethinkKey     = flag.String("rethinkdb_key", "", "Key of the RethinkDB connection")
 	rethinkAPIName = flag.String("rethinkdb_api_name", "prod", "Name of the API's database")
 
+	kiriAddresses = flag.String("kiri_addresses", "", "Addresses of the etcd servers to use")
+
+	kiriDiscoveryStores    = flag.String("kiri_discovery_stores", "", "Stores list for service discovery. Syntax: kind,path;kind,path")
+	kiriDiscoveryRethinkDB = flag.String("kiri_discovery_rethinkdb", "rethinkdb", "Name of the RethinkDB service in SD")
+
+	kiriThisStores  = flag.String("kiri_this_stores", "", "Stores list for http backend registering. Syntax: kind,path;kind,path")
+	kiriThisName    = flag.String("kiri_this_name", "invite-api", "Name of the HTTP service")
+	kiriThisAddress = flag.String("kiri_this_address", "", "Address of this service")
+
 	session *r.Session
 )
 
@@ -23,12 +34,63 @@ func main() {
 
 	flag.Parse()
 
+	// Parse kiri addresses
+	ka := strings.Split(*kiriAddresses, ",")
+
+	// Set up kiri agent for discovery
+	kd := kiri.New(ka)
+
+	// Set up a kiri agent for backend
+	kb := kiri.New(ka)
+
+	// Register this service in kb and add stores
+	kb.Register(*kiriThisName, *kiriThisAddress, nil)
+	for i, store := range strings.Split(*kiriThisStores, ";") {
+		parts := strings.Split(store, ",")
+		if len(parts) != 2 {
+			log.Fatalf("Invalid parts count in kiri_this_stores#%d", i)
+		}
+
+		var kind kiri.Format
+		switch parts[0] {
+		case "default":
+			kind = kiri.Default
+		case "puro":
+			kind = kiri.Puro
+		default:
+			log.Fatalf("Invalid kind of store in kiri_this_stores#%d", i)
+		}
+		kb.Store(kind, parts[1])
+	}
+
+	// Add stores to kd
+	for i, store := range strings.Split(*kiriDiscoveryStores, ";") {
+		parts := strings.Split(store, ",")
+		if len(parts) != 2 {
+			log.Fatalf("Invalid parts count in kiri_discovery_stores#%d", i)
+		}
+
+		var kind kiri.Format
+		switch parts[0] {
+		case "default":
+			kind = kiri.Default
+		case "puro":
+			kind = kiri.Puro
+		default:
+			log.Fatalf("Invalid kind of store in kiri_discovery_stores#%d", i)
+		}
+		kd.Store(kind, parts[1])
+	}
+
 	// Connect to RethinkDB
-	var err error
-	session, err = r.Connect(r.ConnectOpts{
-		Address: *rethinkAddress,
-		AuthKey: *rethinkKey,
-	})
+	var session *r.Session
+	err := kd.Discover(*kiriDiscoveryRethinkDB, nil, kiri.DiscoverFunc(func(service *kiri.Service) error {
+		var err error
+		session, err = r.Connect(r.ConnectOpts{
+			Address: service.Address,
+		})
+		return err
+	}))
 	if err != nil {
 		log.Fatal(err)
 	}
